af73e27bde7c12f53c5caf16dd00903d45956ec3
Keegan Wright <keeganwright12@gmail.com>
2025-08-22 16:31:55 +0100

    Stops reloading of free games if loaded in x time period

--- YASAM.ViewModels/FreeGames/FreeGamesViewModel.cs
+++ YASAM.ViewModels/FreeGames/FreeGamesViewModel.cs
@@ -16,6 +16,10 @@ public partial class FreeGamesViewModel : PageViewModelBase
     private ObservableCollection<GameViewModel> _freeGames = [];
     
     [ObservableProperty] private bool _loading;
+    
+    
+    [ObservableProperty] private DateTimeOffset? _lastUpdated;
+    
     public sealed override string DisplayName { get; init; }
     
     public FreeGamesViewModel(ISteamStoreClient steamStoreClient)
@@ -26,7 +30,7 @@ public partial class FreeGamesViewModel : PageViewModelBase
     }
     
     [RelayCommand]
-    public void ShowInSteam(GameViewModel vm)
+    private void ShowInSteam(GameViewModel vm)
     {
         _steamStoreClient.OpenStorePage(vm.AppId, vm.Name);
     }
@@ -35,26 +39,28 @@ public partial class FreeGamesViewModel : PageViewModelBase
     private async Task LoadAsync()
     {
         Loading = true;
-        
-        var freeGames = new List<GameViewModel>();
-        await foreach (var freeGame in _steamStoreClient.GetFreeGamesAsync())
+        if (!LastUpdated.HasValue || DateTimeOffset.UtcNow - LastUpdated?.UtcDateTime > TimeSpan.FromMinutes(5))
         {
-            freeGames.Add(new GameViewModel(freeGame.AppId, freeGame!.Name!, 0));
-        }
+            
+            var freeGames = new List<GameViewModel>();
+            await foreach (var freeGame in _steamStoreClient.GetFreeGamesAsync())
+            {
+                freeGames.Add(new GameViewModel(freeGame.AppId, freeGame!.Name!, 0));
+            }
         
-        FreeGames = new ObservableCollection<GameViewModel>(freeGames);
+            FreeGames = new ObservableCollection<GameViewModel>(freeGames);
 
-        if (freeGames.Any())
-        {
-            var toastManager = Ioc.Default.GetRequiredService<ISukiToastManager>();
-            toastManager.CreateToast()
-                .OfType(NotificationType.Information)
-                .WithContent($"{freeGames.Count} free game(s) available.")
-                .Dismiss().After(TimeSpan.FromSeconds(3))
-                .Dismiss().ByClicking()
-                .Queue();   
+            if (freeGames.Any())
+            {
+                var toastManager = Ioc.Default.GetRequiredService<ISukiToastManager>();
+                toastManager.CreateToast()
+                    .OfType(NotificationType.Information)
+                    .WithContent($"{freeGames.Count} free game(s) available.")
+                    .Dismiss().After(TimeSpan.FromSeconds(3))
+                    .Dismiss().ByClicking()
+                    .Queue();   
+            }
         }
-        
         Loading = false;
     }
 }
