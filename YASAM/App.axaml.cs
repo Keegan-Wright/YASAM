26a30119a9fb50405cae415081acc8a5ed87c937
Keegan Wright <keeganwright12@gmail.com>
2025-09-03 23:01:51 +0100

    Service clean up

--- YASAM/App.axaml.cs
+++ YASAM/App.axaml.cs
@@ -35,38 +35,25 @@ public class App : Application
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
             var services = new ServiceCollection();
-            services.AddSingleton(desktop);
-
+            
             Batteries.Init();
+            
+            services.AddSingleton(desktop);
             services.AddDbContext<YasamDbContext>();
-
-
+            services.AddSingleton<ISukiDialogManager, SukiDialogManager>(_ => new SukiDialogManager());
+            services.AddSingleton<ISukiToastManager, SukiToastManager>(_ => new SukiToastManager());
+            services.AddMemoryCache();
+            
             AddWindows(services);
             AddViews(services);
             AddViewModels(services);
             AddServices(services);
-
-            services.AddSingleton<ISukiDialogManager, SukiDialogManager>(_ => new SukiDialogManager());
-            services.AddSingleton<ISukiToastManager, SukiToastManager>(_ => new SukiToastManager());
-            
-            services.AddMemoryCache();
             
             var provider = services.BuildServiceProvider();
 
             Ioc.Default.ConfigureServices(provider);
 
-
-            var db = Ioc.Default.GetRequiredService<YasamDbContext>();
-
-            try
-            {
-                db.Database.Migrate();
-            }
-            catch
-            {
-                db.Database.EnsureCreated();
-            }
-
+            MigrateDatabase();
 
             // Avoid duplicate validations from both Avalonia and the CommunityToolkit. 
             // More info: https://docs.avaloniaui.net/docs/guides/development-guides/data-validation#manage-validationplugins
@@ -94,6 +81,20 @@ public class App : Application
         base.OnFrameworkInitializationCompleted();
     }
 
+    private void MigrateDatabase()
+    {
+        var db = Ioc.Default.GetRequiredService<YasamDbContext>();
+
+        try
+        {
+            db.Database.Migrate();
+        }
+        catch
+        {
+            db.Database.EnsureCreated();
+        }
+
+    }
     private static void AddViewModels(ServiceCollection services)
     {
         services.AddSingleton<MainWindowViewModel>();
